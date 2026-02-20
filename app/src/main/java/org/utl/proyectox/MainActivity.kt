65f6352543460b08fe6da97743ccf41a3c7ad301
420miguel <josemiguelaguirremendiola@gmail.com>
2026-02-20 16:58:55 -0600

    Actualize el codigo ahora el usuario ingresa residuos y el recolector los puede vizualizar

--- app/src/main/java/org/utl/proyectox/MainActivity.kt
+++ app/src/main/java/org/utl/proyectox/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.utl.proyectox.ui.ListaResiduosFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,47 +14,56 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        // Recibimos el ROL que mandaste desde el Login
-        val rol = intent.getStringExtra("ROL")
+        // 🔥 LEEMOS EL ROL DESDE SHARED PREFERENCES
+        val sharedPref = getSharedPreferences("sesion", MODE_PRIVATE)
+        val rol = sharedPref.getString("ROL", "CIUDADANO")
 
-        // 1. Configuración inicial según el ROL
-        if (rol == "RECOLECTOR") {
+        // 🔥 Normalizamos por si viene en minúsculas
+        val rolNormalizado = rol?.uppercase()
+
+        if (rolNormalizado == "RECOLECTOR") {
+
             bottomNav.menu.clear()
             bottomNav.inflateMenu(R.menu.bottom_menu_collector)
-            // CAMBIO: El recolector ahora inicia en la pantalla del MAPA
+
             cambiarPantalla(HomeRecolectorFragment())
+
         } else {
+
             bottomNav.menu.clear()
             bottomNav.inflateMenu(R.menu.bottom_menu_citizen)
-            // El ciudadano inicia en su bienvenida con el botón verde
+
             cambiarPantalla(HomeCiudadanoFragment())
         }
 
-        // 2. Navegación: ¿A dónde ir cuando tocan un icono?
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                // Inicio para Ciudadano
+
                 R.id.nav_home -> {
                     cambiarPantalla(HomeCiudadanoFragment())
                     true
                 }
-                // Inicio para Recolector (Mapa)
+
                 R.id.nav_home_collector -> {
                     cambiarPantalla(HomeRecolectorFragment())
                     true
                 }
+
                 R.id.nav_report -> {
                     cambiarPantalla(ReporteFragment())
                     true
                 }
+
                 R.id.nav_waste -> {
                     cambiarPantalla(ListaResiduosFragment())
                     true
                 }
+
                 R.id.nav_profile -> {
                     cambiarPantalla(PerfilFragment())
                     true
                 }
+
                 else -> false
             }
         }
